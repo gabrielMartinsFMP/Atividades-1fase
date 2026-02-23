fafe70900cd718b273ad7d58ea4f3c911a56d7c4
gabriel <gabriel14em@gamil.com>
2026-02-23 11:04:20 -0300

    atividades de python com input e print

---
+++ ProgramacaoEstruturada/Aula1_/1.py
@@ -0,0 +1,5 @@
+marca = 'Nissan'
+ano = 2018
+motor = 2.0
+
+print("O carro é um", marca, ", do ano de", ano, "e de motor", motor, ".")
---
+++ ProgramacaoEstruturada/Aula1_/2.py
@@ -0,0 +1,2 @@
+nome = input("Qual seu nome?")
+print("Ahhh sim... Seja bem-vindo(a) a FMP!", nome)
